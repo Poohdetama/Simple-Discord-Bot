5c612cefd5b3b5a5a03dd4a7c2d7aa24adbae93d
Poohdetama <105565591+Poohdetama@users.noreply.github.com>
2022-05-15 18:50:02 -0500

    Create poohli_list.py

---
+++ poohli_list.py
@@ -0,0 +1,4 @@
+phrases = ('Your reflex arc is slower than a snail.', 'Dumb, dumb, dumb!', "You're a unicellular organism!", 'Poohblind!', 'Neutered!', "You're so girly!"
+)
+
+laughs = ('Hahahahahaha!', 'LOLOLOLOL', 'LOL', 'Ahahahahaha!')
